c79125dfa731ea1165fe14dd8cb6e6f30fb5c0e7
sinakhezrian <sina.khezrian.one@gmail.com>
2026-05-04 16:44:03 +0200

    update the env values

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -6,8 +6,8 @@
 
 export const environment = {
   production: true,
-  ws: 'https://127.0.0.1:8000/api/v1/',
-  submission_server: 'https://scheduler.biocomputingup.it/'
+  ws: 'http://127.0.0.1:8000/api/v1/',
+  submission_server: 'http://scheduler.biocomputingup.it/'
 };
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 
 export const environment = {
   production: false,
-  ws: 'https://127.0.0.1:8000/v1/',
-  submission_server: 'https://scheduler.biocomputingup.it/'
+  ws: 'http://127.0.0.1:8000/v1/',
+  submission_server: 'http://scheduler.biocomputingup.it/'
 };
