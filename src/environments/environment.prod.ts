8c4279135777c92febb13770e7036621fc7aef7a
Ivan Mičetić <ivan.micetic@gmail.com>
2026-09-06 02:49:21 +0200

    Update ws to old.deposition

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  ws: 'https://deposition.proteinensemble.org/api/v1/',
+  ws: 'https://old.deposition.proteinensemble.org/api/v1/',
   submission_server: 'https://scheduler.biocomputingup.it/'
 };
 
