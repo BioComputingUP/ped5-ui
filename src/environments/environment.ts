40b2996038ee81412725d4737d6e4d3f67f287a3
Ivan Mičetić <ivan.micetic@gmail.com>
2026-09-07 14:26:06 +0200

    Test local configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  ws: 'https://deposition.proteinensemble.org/api/v1/',
+  ws: 'http://127.0.0.1:8000/v1/',
   submission_server: 'https://scheduler.biocomputingup.it/'
 };
